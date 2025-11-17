a66e03919e9e44696e920f9a0813ba0b99bf361d
Gérald Leban <gerald.leban0@gmail.com>
2025-11-17 07:25:46 +0100

    test: check the password is hashed before persisting the user

--- tests/functional/authentication/register.spec.ts
+++ tests/functional/authentication/register.spec.ts
@@ -1,6 +1,24 @@
+import User from '#authentication/models/user'
+import { UserFactoryWithNoPassword } from '#database/factories/user_factory'
+import hash from '@adonisjs/core/services/hash'
 import { test } from '@japa/runner'
 
-test.group('Authentication register', () => {
-  test('example test', async ({ assert }) => {
+test.group('creating user', (group) => {
+  group.each.setup(async () => {
+    await User.query().delete()
+  })
+
+  group.each.teardown(async () => {
+    await User.query().delete()
+  })
+
+  test('hashes user password', async ({ assert }) => {
+    const user = await UserFactoryWithNoPassword.make()
+    user.password = 'password'
+
+    await user.save()
+
+    assert.isTrue(hash.isValidHash(user.password))
+    assert.isTrue(await hash.verify(user.password, 'password'))
   })
 })
