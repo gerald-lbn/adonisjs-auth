b776f49ceb692cef83dce3c92c5829af822b17bf
Gérald Leban <gerald.leban0@gmail.com>
2025-11-18 00:05:39 +0100

    feat: wrap app with theme provider

--- inertia/app/app.tsx
+++ inertia/app/app.tsx
@@ -1,10 +1,11 @@
 /// <reference path="../../adonisrc.ts" />
 /// <reference path="../../config/inertia.ts" />
 
-import '../css/app.css'
-import { hydrateRoot } from 'react-dom/client'
-import { createInertiaApp } from '@inertiajs/react'
 import { resolvePageComponent } from '@adonisjs/inertia/helpers'
+import { createInertiaApp } from '@inertiajs/react'
+import { hydrateRoot } from 'react-dom/client'
+import { ThemeProvider } from '~/providers/theme_provider'
+import '../styles/globals.css'
 
 const appName = import.meta.env.VITE_APP_NAME || 'AdonisJS'
 
@@ -18,6 +19,11 @@ createInertiaApp({
   },
 
   setup({ el, App, props }) {
-    hydrateRoot(el, <App {...props} />)
+    hydrateRoot(
+      el,
+      <ThemeProvider>
+        <App {...props} />
+      </ThemeProvider>
+    )
   },
 })
